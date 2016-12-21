2f132c79c3d327e55cf75898e5b477d3d414c510
LostInDreamZ <lostindreamzde@gmail.com>
2016-12-21 23:53:31 +0100

    HUD Test

--- !TanoaLifeProjekt/xerlutas_life.tanoa/dialog/hud_stats.hpp
+++ !TanoaLifeProjekt/xerlutas_life.tanoa/dialog/hud_stats.hpp
@@ -25,64 +25,64 @@ class playerHUD {
     };
 
     /* Background */
-    class Life_RscBackground_HUD: Life_RscBackground {
-        colorBackground[] = {0,0,0,0.35};
-        x = 0.414815 * safezoneW + safezoneX;
-        y = 0.966667 * safezoneH + safezoneY;
-        w = 0.170371 * safezoneW;
-        h = 0.0333333 * safezoneH;
+    class Life_RscBackground_HUD: Life_RscPicture {
+        text = "dialog\CustomHUD\hud.paa";
+        x = 0.863576 * safezoneW + safezoneX;
+        y = 0.759814 * safezoneH + safezoneY;
+        w = 0.125 * safezoneW;
+        h = 0.222222 * safezoneH;
     };
 
     /* Progress Bars */
     class LIFE_RscProgress_HUDCommon: Life_RscProgress {
         colorFrame[] = {0, 0, 0, 0.8};
-        y = 0.972223 * safezoneH + safezoneY;
-        w = 0.0462964 * safezoneW;
-        h = 0.0222222 * safezoneH;
+        x = 0.876852 * safezoneH + safezoneX;
+        w = 0.096296 * safezoneW;
+        h = 0.052222 * safezoneH;
     };
 
     class Life_RscProgress_HUDFood: LIFE_RscProgress_HUDCommon {
         idc = 2200;
         colorBar[] = {0,0.50,0,0.65};
-        x = 0.418981 * safezoneW + safezoneX;
+        y = 0.842223 * safezoneW + safezoneY;
     };
 
     class Life_RscProgress_HUDHealth: LIFE_RscProgress_HUDCommon {
         idc = 2201;
         colorBar[] = {0.85,0.05,0,0.65};
-        x = 0.476852 * safezoneW + safezoneX;
+        y = 0.772223 * safezoneW + safezoneY;
     };
 
     class Life_RscProgress_HUDWater: LIFE_RscProgress_HUDCommon {
         idc = 2202;
         colorBar[] = {0,0.25,0.65,0.65};
-        x = 0.534723 * safezoneW + safezoneX;
+        y = 0.912223 * safezoneW + safezoneY;
     };
 
     /* Texts */
     class Life_RscText_HUDCommon: Life_RscText {
         SizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.8)";
         style = ST_CENTER;
-        y = 0.970023 * safezoneH + safezoneY;
-        w = 0.0462964 * safezoneW;
-        h = 0.0222222 * safezoneH;
+        x = 0.876852 * safezoneH + safezoneX;
+        w = 0.096296 * safezoneW;
+        h = 0.222222 * safezoneH;
     };
 
     class Life_RscText_HUDFood: Life_RscText_HUDCommon {
         idc = 1200;
         text = "$STR_HUD_Food";
-        x = 0.418981 * safezoneW + safezoneX;
+        y = 0.842223 * safezoneW + safezoneY;
     };
 
     class Life_RscText_HUDHealth: Life_RscText_HUDCommon {
         idc = 1201;
         text = "$STR_HUD_Health";
-        x = 0.476852 * safezoneW + safezoneX;
+        y = 0.772223 * safezoneW + safezoneY;
     };
 
     class Life_RscText_HUDWater: Life_RscText_HUDCommon {
         idc = 1202;
         text = "$STR_HUD_Water";
-        x = 0.534723 * safezoneW + safezoneX;
+        y = 0.912223 * safezoneW + safezoneY;
     };
 };
