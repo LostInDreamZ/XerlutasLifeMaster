860a7116b9e16a9ec4fc94e608fca834dfb834e3
LostInDreamZ <lostindreamzde@gmail.com>
2016-12-22 09:54:38 +0100

    Smartphone
    Zweite Schritte

--- !TanoaLifeProjekt/xerlutas_life.tanoa/dialog/player_inv.hpp
+++ !TanoaLifeProjekt/xerlutas_life.tanoa/dialog/player_inv.hpp
@@ -4,167 +4,167 @@ class playerSettings {
     enableSimulation = 1;
 
     class controlsBackground {
-		class Picturebox_1100: Life_RscPicture
+		class XerlutasSmartphone: RscPicture //OK
         {
             idc = 1100;
             text = "dialog\CustomSmartPhone\HandyHauptMenu.paa";
-            x = 0.686964 * safezoneW + safezoneX;
-            y = 0.499064 * safezoneH + safezoneY;
-            w = 0.319402 * safezoneW;
-            h = 0.501603 * safezoneH;
+            x = 0.598954 * safezoneW + safezoneX;
+            y = 0.289352 * safezoneH + safezoneY;
+            w = 0.409382 * safezoneW;
+            h = 0.712529 * safezoneH;
         };
     };
     class controls {
-        class ButtonClose: Life_RscButtonMenu //Ok
+        class ButtonClose: Life_RscButtonInvisible  //OK
         {
             idc = -1;
             text = "Close";
 			onButtonClick = "closeDialog 0;";
-            x = 0.750831 * safezoneW + safezoneX;
-            y = 0.888071 * safezoneH + safezoneY;
-            w = 0.118130 * safezoneW;
-            h = 0.038088 * safezoneH;
+            x = 0.679335 * safezoneW + safezoneX;
+            y = 0.840492 * safezoneH + safezoneY;
+            w = 0.154175 * safezoneW;
+            h = 0.053272 * safezoneH;
         };
-        class ButtonSettings: Life_RscButtonMenu //Ok
+        class ButtonSettings: Life_RscButtonInvisible  //OK
         {
             idc = -1;
             text = "$STR_Global_Settings";
             onButtonClick = "[] call life_fnc_settingsMenu;";
-            x = 0.752982 * safezoneW + safezoneX;
-            y = 0.834112 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.683616 * safezoneW + safezoneX;
+            y = 0.763762 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
-        class Button_Sync: Life_RscButtonMenu //OK
+        class Button_Sync: Life_RscButtonInvisible  //OK
         {
             idc = -1;
             //shortcuts[] = {0x00050000 + 2};
             text = "$STR_PM_SyncData";
             onButtonClick = "[] call SOCK_fnc_syncData;";
-            x = 0.783492 * safezoneW + safezoneX;
-            y = 0.833444 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.721116 * safezoneW + safezoneX;
+            y = 0.763762 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
-        class Button_SMS: Life_RscButtonMenu //OK
+        class Button_SMS: Life_RscButtonInvisible  //OK
         {
             idc = 2014;
             text = "$STR_PM_CellPhone";
             onButtonClick = "createDialog ""Life_cell_phone"";";
-            x = 0.814003 * safezoneW + safezoneX;
-            y = 0.833444 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.758616 * safezoneW + safezoneX;
+            y = 0.764997 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
-        class Button_Cash: Life_RscButtonMenu
+        class Button_Cash: Life_RscButtonInvisible  //Implementierung folgt!
         {
             idc = 1103;
             text = "Button";
-            x = 0.843340 * safezoneW + safezoneX;
-            y = 0.833444 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.796116 * safezoneW + safezoneX;
+            y = 0.764997 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
             ColorBackground[] = {0,0,0,0.627450};
             ColorText[] = {1,1,1,1};
             font = TahomaB;
             colorFocused[] = {0,0,0,1};
             colorDisabled[] = {0,0,0,1};
         };
-        class Button_Inv: Life_RscButtonMenu
+        class Button_Inv: Life_RscButtonInvisible  //Implementierung folgt!
         {
             idc = 1101;
             text = "Button";
-            x = 0.753764 * safezoneW + safezoneX;
-            y = 0.584196 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.681532 * safezoneW + safezoneX;
+            y = 0.408207 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
             ColorBackground[] = {0,0,0,0.627450};
             ColorText[] = {1,1,1,1};
             font = TahomaB;
             colorFocused[] = {0,0,0,1};
             colorDisabled[] = {0,0,0,1};
         };
-        class Button_keys: Life_RscButtonMenu
+        class Button_keys: Life_RscButtonInvisible  //OK
         {
             idc = 2013;
             text = "$STR_PM_KeyChain";
             onButtonClick = "createDialog ""Life_key_management"";";
-            x = 0.783492 * safezoneW + safezoneX;
-            y = 0.584196 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.719727 * safezoneW + safezoneX;
+            y = 0.408207 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
-        class Button_license: Life_RscButtonMenu
+        class Button_license: Life_RscButtonInvisible  //Implementierung folgt!
         {
             idc = 1108;
             text = "Button";
-            x = 0.814003 * safezoneW + safezoneX;
-            y = 0.584196 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.758616 * safezoneW + safezoneX;
+            y = 0.409441 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
             ColorBackground[] = {0,0,0,0.627450};
             ColorText[] = {1,1,1,1};
             font = TahomaB;
             colorFocused[] = {0,0,0,1};
             colorDisabled[] = {0,0,0,1};
         };
-        class Button_Gang: Life_RscButtonMenu //Ok
+        class Button_Gang: Life_RscButtonInvisible  //OK
         {
             idc = 2011;
             text = "$STR_PM_MyGang";
             onButtonClick = "if (isNil ""life_action_gangInUse"") then {if (isNil {(group player) getVariable ""gang_owner""}) then {createDialog ""Life_Create_Gang_Diag"";} else {[] spawn life_fnc_gangMenu;};};";
-            x = 0.843340 * safezoneW + safezoneX;
-            y = 0.584196 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.797505 * safezoneW + safezoneX;
+            y = 0.409441 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
-        class Button_dance: Life_RscButtonMenu
+        class Button_dance: Life_RscButtonInvisible  //Implementierung folgt!
         {
             idc = 1110;
             text = "Button";
-            x = 0.753764 * safezoneW + safezoneX;
-            y = 0.633645 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.682227 * safezoneW + safezoneX;
+            y = 0.477343 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
             ColorBackground[] = {0,0,0,0.627450};
             ColorText[] = {1,1,1,1};
             font = TahomaB;
             colorFocused[] = {0,0,0,1};
             colorDisabled[] = {0,0,0,1};
         };
-        class Button_market: Life_RscButtonMenu
+        class Button_market: Life_RscButtonInvisible  //Implementierung folgt!
         {
             idc = 1111;
             text = "Button";
-            x = 0.783884 * safezoneW + safezoneX;
-            y = 0.632977 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.721116 * safezoneW + safezoneX;
+            y = 0.478577 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
             ColorBackground[] = {0,0,0,0.627450};
             ColorText[] = {1,1,1,1};
             font = TahomaB;
             colorFocused[] = {0,0,0,1};
             colorDisabled[] = {0,0,0,1};
         };
-        class Button_wanted: Life_RscButtonMenu
+        class Button_wanted: Life_RscButtonInvisible 
         {
             idc = 2012;
             text = "$STR_PM_WantedList";
             onButtonClick = "[] call life_fnc_wantedMenu";
-            x = 0.753373 * safezoneW + safezoneX;
-            y = 0.727865 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.681532 * safezoneW + safezoneX;
+            y = 0.609441 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
-        class Button_admin: Life_RscButtonMenu
+        class Button_admin: Life_RscButtonInvisible 
         {
-            idc = 2021;
+			idc = 2021;
             text = "$STR_PM_AdminMenu";
             onButtonClick = "closeDialog 0; createDialog ""life_admin_menu"";";
-            x = 0.753373 * safezoneW + safezoneX;
-            y = 0.775977 * safezoneH + safezoneY;
-            w = 0.024643 * safezoneW;
-            h = 0.035081 * safezoneH;
+            x = 0.682227 * safezoneW + safezoneX;
+            y = 0.678577 * safezoneH + safezoneY;
+            w = 0.035892 * safezoneW;
+            h = 0.067412 * safezoneH;
         };
     };
 };
